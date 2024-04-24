f5ab19d02f68188b5277af48937edc1b21772329
XIN LYU <nicolexin416@gmail.com>
2024-04-24 17:51:39 -0400

    fix cors issue

--- server.js
+++ server.js
@@ -10,7 +10,15 @@ connectDB()
 
 const app = express();
 
-app.use(cors())
+const corsOptions = {
+  allowedHeaders: ["authorization", "Content-Type"], // you can change the headers
+  exposedHeaders: ["authorization"], // you can change the headers
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false
+}
+
+app.use(cors(corsOptions))
 
 app.use(express.json()); // Middleware to parse JSON bodies
 
